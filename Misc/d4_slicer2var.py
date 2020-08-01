993faa920a91adbc619501138f87c0bcccb562b8
Mahnoor Anjum <manomaq@gmail.com>
2020-08-02 01:08:12 +0500

    added file

---
+++ Misc/d4_slicer2var.py
@@ -0,0 +1,21 @@
+'''
+    Mahnoor Anjum
+    Python:
+        Bivariate Analysis
+'''
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import math
+import random 
+
+path = 'data/private/savepath/'
+filename = 'v2_1'
+savepath = 'data/private/saveslice/'
+data = pd.read_csv(path + filename+'.csv')
+
+for i in [500, 1000, 2000, 3000, len(data.index)]:
+    _temp = data.sample(n=i)
+    _temp.to_csv(savepath + filename + '_' + str(i) + '.csv', index=None)
+    
