1e3395792ba6c9b05ec920ea33633c585b2dd480
Mahnoor Anjum <manomaq@gmail.com>
2020-08-18 22:21:50 +0500

    added file

---
+++ Clustering_Snippets/T6_getClusters_optics.py
@@ -0,0 +1,35 @@
+'''
+    Mahnoor Anjum
+    Python:
+        Clusters
+'''
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import math
+import random 
+
+filename = 'private'
+path = 'data/' + filename + '.txt'
+data = pd.read_csv(path, delimiter='\t')
+
+columns = ['X','Y','Z']
+X = data[columns]
+
+
+data.isnull().sum()
+
+from sklearn.preprocessing import MinMaxScaler
+obj = MinMaxScaler()
+X_scaled = obj.fit_transform(X)
+
+from sklearn.cluster import OPTICS
+model = OPTICS()
+y_pred = model.fit_predict(X_scaled)
+
+clusters = np.unique(y_pred)
+data = X.join(pd.DataFrame({'Cluster':y_pred}))
+data.to_csv('data/clusters/' + filename  + '.csv', index=None)
+
