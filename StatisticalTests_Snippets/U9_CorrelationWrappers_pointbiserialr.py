9d05e65a974eb5fe296b9d0f6335361afcc6ef14
Mahnoor Anjum <manomaq@gmail.com>
2020-09-12 00:08:41 +0500

    added file

---
+++ StatisticalTests_Snippets/U9_CorrelationWrappers_pointbiserialr.py
@@ -0,0 +1,31 @@
+'''
+    Implementation: Mahnoor Anjum 
+    Description:
+        Intersection Test
+    
+    By:
+        www.geeksforgeeks.org
+
+    
+'''
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy import stats
+
+data = pd.read_csv('data/combined_kmeans25_100.csv')
+
+method = 'pointbiserialr'
+def custom(a, b):
+    v,p = stats.pointbiserialr(a, b)
+    return round(v, 4)
+
+corr_mat = data.corr(method = custom)
+
+fig, ax = plt.subplots(1,1, figsize = (10,4))
+ax = sns.heatmap(corr_mat, cmap = 'YlGnBu', linewidths=.5, annot=True)
+ax.set_title(str(method))
+plt.savefig(str(method) + '.png')
