700f233e37dcc5efd647c55fb61516462748dc7d
Mahnoor Anjum <manomaq@gmail.com>
2021-12-09 08:59:13 +0500

    added cost

--- DSA_Snippets/ds_dynamicArray.py
+++ DSA_Snippets/ds_dynamicArray.py
@@ -1,6 +1,19 @@
+'''
+    When dynamic arrays are full they double 
+    its called amortization 
+    we double up 
+    
+    double on overflow
+    O(1) on each append without overflow 
+    
+    item  1 2 3 4 5 6 7 8 09 10 11 12 13 14 15 16 17
+    size  1 2 4 4 8 8 8 8 16 16 16 16 16 16 16 16 32
+    cost  1 2 3 1 5 1 1 1 09  1  1  1  1  1  1  1 17
+    
+    
+'''
 
 import ctypes 
-
 
 class DynamicArray(object):
     
