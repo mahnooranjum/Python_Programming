5ccd77f41a0d90bf010cd9ea5ff980544df2020c
Mahnoor Anjum <manomaq@gmail.com>
2021-12-10 15:10:15 +0500

    added

---
+++ DSA_Snippets/ds_doublyLinked.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: MAQ
+"""
+
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+        self.prev  = None
+        
+        
+a = Node(1)
+b = Node(2)  
+c = Node(3)
+
+a.next = b
+b.prev = a
+b.next = c
+c.prev = b
+
+a.next.next.value
+c.prev.prev.value
