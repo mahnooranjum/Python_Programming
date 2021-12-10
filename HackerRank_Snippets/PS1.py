d91be6038b51118362a9b61226eb0408e39e63dc
Mahnoor Anjum <manomaq@gmail.com>
2021-12-10 22:21:33 +0500

    added

---
+++ HackerRank_Snippets/PS1.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count = 0
+    uniques = []
+    for i in ar:
+        print(uniques)
+        if i not in uniques:
+            uniques.append(i)
+        else:
+            count+=1
+            uniques.remove(i)
+    return count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
