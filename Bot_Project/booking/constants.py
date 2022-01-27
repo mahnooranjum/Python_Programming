f565c904640c7df547a1d7617ef0da26f48e7101
Mahnoor Anjum <manomaq@gmail.com>
2022-01-27 13:30:05 +0500

    added constants

---
+++ Bot_Project/booking/constants.py
@@ -0,0 +1,3 @@
+BASE_URL = 'https://www.booking.com'
+# EDGE_PATH = "E:/WorkFiles/Github/Python_Programming/Subject_Automation/seleniumDrivers/msedgedriver.exe"
+EDGE_PATH = "E:/WorkFiles/Github/Python_Programming/Subject_Automation/seleniumDrivers"
