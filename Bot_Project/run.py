437f95c19d424473a543ee5ad270e5ec32696996
Mahnoor Anjum <manomaq@gmail.com>
2022-01-28 05:22:30 +0500

    added price logger

--- Bot_Project/run.py
+++ Bot_Project/run.py
@@ -6,9 +6,12 @@ from booking.booking import Booking
 
 with Booking(teardown = False) as bot:
     bot.land_first_page()
-    bot.change_language(lang='en-gb')
-    bot.change_currency(currency='PKR')
+    # bot.change_language(lang='en-gb')
+    # bot.change_currency(currency='PKR')
     bot.search_place(place='Islamabad')
-    bot.select_dates(checkin="2022-01-28", checkout="2022-01-29")
-    bot.select_adults(10)
+    bot.select_dates(checkin="2022-01-29", checkout="2022-01-30")
+    bot.select_adults(3)
     bot.click_search()
+    bot.apply_filter()
+    bot.refresh()
+    bot.show_results()
