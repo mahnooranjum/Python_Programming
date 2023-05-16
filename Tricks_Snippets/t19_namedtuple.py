2008b267b58ef160f928f1e738ea8d4773259caf
Mahnoor Anjum <manomaq@gmail.com>
2023-05-16 06:12:22 +0500

    added

---
+++ Tricks_Snippets/t19_namedtuple.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 05:43:01 2023
+
+@author: MAQ
+"""
+
+from collections import namedtuple
+
+
+Car = namedtuple('Car', 'color model')
+
+my_car = Car('red', 'toyota')
+
+
+my_car._fields
+
+my_car._asdict()
+
+my_car.replace(color='blue')
